405d5873155842c6fae4128981518cd53ec6c968
alexharris <alexharris6@gmail.com>
2014-08-19 20:39:56 -0700

    tweak a few styles, add mailchimp footer widget

--- footer.php
+++ footer.php
@@ -11,12 +11,15 @@
 	</section><!-- #content -->
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="row">
-            <a class="footer-contact" href="#">openengagement@gmail.com</a>
-            <p class="footer-newsletter">Newsletter <input type="text"></input></p>
+            <span class="footer-contact">
+                <h3>Contact</h3>
+                <a href="#">openengagement@gmail.com</a>
+            </span>
+            <p class="footer-newsletter"><?php mailchimpSF_signup_form(); ?></p>
             <ul class="social-media">
-                <li><img src="<?php echo get_template_directory_uri(); ?>/img/facebook.png" /></li>
-                <li><img src="<?php echo get_template_directory_uri(); ?>/img/instagram.png" /></li>
-                <li><img src="<?php echo get_template_directory_uri(); ?>/img/twitter.png" /></li>
+                <li><a href="https://www.facebook.com/openengagementconference"><img src="<?php echo get_template_directory_uri(); ?>/img/facebook.png" /></a></li>
+                <li><a href="http://instagram.com/openengagement"><img src="<?php echo get_template_directory_uri(); ?>/img/instagram.png" /></a></li>
+                <li><a href="https://twitter.com/OPENENGAGEMENT"><img src="<?php echo get_template_directory_uri(); ?>/img/twitter.png" /></a></li>
             </ul>
         </div>
 	</footer><!-- #colophon -->
