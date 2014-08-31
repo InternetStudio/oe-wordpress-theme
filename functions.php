3b4e81450e13f6a1b9d88e7268f9d8d55aa33d74
alexharris <alexharris6@gmail.com>
2014-08-30 20:53:29 -0700

    add mobile footer alignment and padding, remove post formats

--- functions.php
+++ functions.php
@@ -58,9 +58,9 @@ function oewebsite_setup() {
 	 * Enable support for Post Formats.
 	 * See http://codex.wordpress.org/Post_Formats
 	 */
-	add_theme_support( 'post-formats', array(
-		'aside', 'image', 'video', 'quote', 'link'
-	) );
+	// add_theme_support( 'post-formats', array(
+	// 	'aside', 'image', 'video', 'quote', 'link'
+	// ) );
 
 	// Setup the WordPress core custom background feature.
 	add_theme_support( 'custom-background', apply_filters( 'oewebsite_custom_background_args', array(
