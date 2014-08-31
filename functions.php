4b664159ac07c85073534ccf99ac940677853d5f
alexharris <alexharris6@gmail.com>
2014-08-30 20:11:20 -0700

    margins, etc

--- functions.php
+++ functions.php
@@ -214,4 +214,14 @@ function addColumn ($attr, $content) {
 
 add_shortcode('column', 'addColumn');
 
+// REMOVE insertion of width/height from images
+
+add_filter( 'post_thumbnail_html', 'remove_width_attribute', 10 );
+add_filter( 'image_send_to_editor', 'remove_width_attribute', 10 );
+
+function remove_width_attribute( $html ) {
+   $html = preg_replace( '/(width|height)="\d*"\s/', "", $html );
+   return $html;
+}
+
 
