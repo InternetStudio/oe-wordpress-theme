18791909f8733baa629df31fdde23038a947d635
alexharris <alexharris6@gmail.com>
2014-09-01 17:39:04 -0700

    reposition 'top' button, add mobile blog header image

--- category-2015.php
+++ category-2015.php
@@ -8,7 +8,8 @@
  */
 
 get_header(); ?>
-	<img src="<?php echo get_template_directory_uri(); ?>/img/blog-top.jpg" />
+	<img class="blog-top" src="<?php echo get_template_directory_uri(); ?>/img/blog-top.jpg" />
+	<img class="blog-top-mobile" src="<?php echo get_template_directory_uri(); ?>/img/blog-top-mobile.jpg" />
 	<?php if ( have_posts() ) : ?>
 
 		<?php /* Start the Loop */ ?>
