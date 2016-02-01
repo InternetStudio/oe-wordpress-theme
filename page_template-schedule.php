2bfa7528e39daa895e420a9390957c9e6de95094
alexharris <alexharris6@gmail.com>
2016-01-31 19:11:27 -0800

    add additional to schedule

--- page_template-schedule.php
+++ page_template-schedule.php
@@ -15,7 +15,7 @@ get_header(); ?>
         <?php
 
         $conferenceDays = array(
-            'Thursday' => array('Aesthetics and Politics'),
+            'Thursday' => array('Pre-conference Intensives'),
             'Friday' => array('Open Houses', 'OMCA Friday Night'),
             'Saturday' => array('All Day', 'Morning', 'Lunchtime', 'Afternoon', 'Evening', 'Keynote', 'After Hours'),
             'Sunday' => array('All Day', 'Morning', 'Lunchtime', 'Afternoon', 'Evening', 'Keynote', 'After Hours'),
@@ -27,9 +27,25 @@ get_header(); ?>
                     <a href="javascript:void(0)" class="tab-link"><?php print ($confDay); ?></a>
                     <div class="tab-content">
                         <?php
-                            if($confDay == 'Saturday' OR $confDay == 'Sunday') {
-                                print('<p>Coming soon! Follow us on social media for updates.</p>');
-                            }
+                        if($confDay == 'Thursday') {
+                            print('
+                                   <p>Pre-Conference programming is planned and hosted by select OE local partners.</p>
+                                   <p>OE 2016 programming details will be released throughout the month of February. Please check back, and follow us on social media for the latest updates on OE2016!</p>
+                            ');
+                        } else if($confDay == 'Friday') {
+                            print('
+                                   <p>Open House Tours are programmed by local partners and will highlight the work being done by local artists and organizations in the Bay Area through a variety of dynamic off-site programming.</p>
+                                   <p>Every Friday night OMCA hosts a family-friendly take on a festive night market, featuring live music, food trucks, local beer and wine, and more! On April 29th "Friday Nights @ OMCA" will feature projects by OE 2016 presenters, and serve as the kick-off event for the conference.</p>
+                                   <p>OE 2016 programming details will be released throughout the month of February. Please check back, and follow us on social media for the latest updates on OE2016! </p>
+                            ');
+
+                        }
+                        else if($confDay == 'Saturday' OR $confDay == 'Sunday') {
+                            print('
+                                <p>OE 2016 opens at 9am Saturday and Sunday, with panels, workshops, discussions, projects, and more throughout the day. Keynote lectures will start at 7:30pm.</p>
+                                <p>OE 2016 programming details will be released throughout the month of February. Please check back, and follow us on social media for the latest updates on OE2016! </p>
+                             ');
+                        }
 
                         ?>
                         <div class="expander">
@@ -49,7 +65,9 @@ get_header(); ?>
                 <a href="javascript:void(0)" class="tab-link">OEHQ</a>
                 <div class="tab-content">
                     <div class="expander">
-                        <p> what shall go here </p>
+                        <p>When you arrive at OE 2016, make your first stop the Open Engagement Headquarters OEHQ! OEHQ is the one-stop shop for checking in, picking up OE 2016 merchandise, and getting the most up-to-date information about the conference throughout the weekend.</p>
+
+                        <p>Hours and exact location of OEHQ will be posted before the conference.</p>
                     </div>
 
                 </div>
