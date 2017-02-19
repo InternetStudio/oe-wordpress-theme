e7526224a60b853ca3fc9f80be695cdf0f8b8345
alexharris <alexharris6@gmail.com>
2017-02-19 14:22:58 -0800

    create 2017 calendar

---
+++ page_template-2017schedule.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Template Name: Chicago Schedule
+ *
+ *
+ * @package oewebsite
+ */
+
+get_header(); ?>
+<div class="one-fourth">
+    <?php get_sidebar(); ?>
+</div>
+<div class="three-fourths">
+    <div class="schedule-2016">
+
+        <?php
+
+        $conferenceDays = array(
+            'Thursday' => array('Pre-conference Intensives'),
+            'Friday' => array('Open Houses','Keynote', 'After Hours'),
+            'Saturday' => array('Open Platform', 'All Day', 'Morning', 'Lunchtime', 'Afternoon', 'Evening', 'Keynote', 'After Hours'),
+            'Sunday' => array('Open Platform', 'All Day', 'Morning', 'Lunchtime', 'Afternoon', 'Evening', 'Keynote', 'After Hours'),
+        )
+        ?>
+        <ul class="accordion-tabs">
+            <?php foreach(array_keys($conferenceDays) as $confDay): ?>
+                <li class="tab-header-and-content">
+                    <a href="javascript:void(0)" class="tab-link"><?php print ($confDay); ?></a>
+                    <div class="tab-content">
+                        <?php
+                            if($confDay == 'Saturday' OR $confDay == 'Sunday') {
+                                print('
+                                    <p>OE 2016 opens at 9am Saturday and Sunday, with panels, workshops, discussions, projects, and more throughout the day. Keynote lectures will start at 7:30pm.</p>
+                                 ');
+                            }
+
+                        ?>
+                        <div class="expander">
+                            <?php foreach($conferenceDays[$confDay] as $group): ?>
+                                <div class="js-expander-trigger expander-trigger expander-hidden"><span class="group-title"><?php print $group; ?>&nbsp;</span></div>
+                                <div id="js-expander-content" class="expander-content">
+<!--                                    --><?php //if ($group == "Pre-conference Intensives") {
+//                                        print('<p>Pre-conference programming is planned and hosted by select OE local partners.</p>');
+//                                    } else if ($group == "Open Houses") {
+//                                        print('<p>Open House Tours are programmed by local partners and will highlight the work being done by local artists and organizations in the Bay Area through a variety of dynamic off-site programming.</p>');
+//                                    } else if ($group == "OMCA Friday Night") {
+//                                        print('<p>Every Friday night OMCA hosts a family-friendly take on a festive night market, featuring live music, food trucks, local beer and wine, and more! On April 29th "Friday Nights @ OMCA" will feature projects by OE 2016 presenters, and serve as the kick-off event for the conference.</p>');
+//                                    }
+//                                    ?>
+                                    <?php include( locate_template( 'taxonomy-cr3ativconfcategory-include2017.php' ) ); ?>
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
+
+                    </div>
+                </li>
+
+            <?php endforeach; ?>
+            <li class="tab-header-and-content">
+                <a href="javascript:void(0)" class="tab-link">OEHQ</a>
+                <div class="tab-content">
+                    <div class="expander">
+                        <p>OEHQ 2017 details to come.</p>
+                    </div>
+
+                </div>
+            </li>
+        </ul>
+    </div>
+</div>
+
+
+<?php get_footer(); ?>
---
+++ taxonomy-cr3ativconfcategory-include2017.php
@@ -0,0 +1,129 @@
+<?php get_header(); ?>
+<!-- Start of content wrapper -->
+<div id="cr3ativconference_contentwrapper">
+
+    <!-- Start of content wrapper -->
+    <div class="cr3ativconference_content_wrapper">
+
+        <?php
+//        print '<pre>';
+//        $terms = get_terms( array(
+//            'taxonomy' => 'post_tag',
+//            'hide_empty' => false,
+//        ) );
+//
+//        var_dump($terms);
+//
+//        // var_dump($wp_query->query_vars);
+//        //    var_dump(get_post_meta($post->ID, 'cr3ativconfmeetingdate', $single = true));
+//        print '</pre>';
+        //?>
+
+        <?php
+
+        add_filter('posts_orderby','cr3ativoderby2');
+
+
+        $wp_query = new WP_Query(
+            array(
+                'post_type' => 'cr3ativconference',
+                'cr3ativconfgroup'=>$group,
+                'posts_per_page' => 99999999,
+                'order' => 'ASC',
+                'meta_key' => 'cr3ativconfmeetingdate',
+                'meta_query' => array(
+                    array(
+                        'key' => 'cr3ativconfmeetingdate',
+                    ),
+                    array(
+                        'key' => 'cr3ativ_confstarttime',
+                    ),
+                ),
+                'tax_query' => array(
+                    array(
+                        'taxonomy' => 'cr3ativconfcategory',
+                        'field'    => 'name',
+                        'terms'    => '2017',
+                    ),
+                ),
+            )
+        );
+        remove_filter('posts_orderby','cr3ativoderby2');
+
+        $sessiondate = '';
+        $timeslot = '';
+        while (have_posts()) : the_post(); ?>
+
+
+            <?php
+
+            $cr3ativconfmeetingdate = get_post_meta($post->ID, 'cr3ativconfmeetingdate', $single = true);
+            $confstarttime = get_post_meta($post->ID, 'cr3ativ_confstarttime', $single = true);
+            $confendtime = get_post_meta($post->ID, 'cr3ativ_confendtime', $single = true);
+            $confdisplaystarttime = get_post_meta($post->ID, 'cr3ativ_confdisplaystarttime', $single = true);
+            $confdisplayendtime = get_post_meta($post->ID, 'cr3ativ_confdisplayendtime', $single = true);
+            $conflocation = get_post_meta($post->ID, 'cr3ativ_conflocation', $single = true);
+            $cr3ativ_highlight = get_post_meta($post->ID, 'cr3ativ_highlight', $single = true);
+
+            ?>
+
+            <?php if( $confDay == getSessionDay($cr3ativconfmeetingdate)): ?>
+                <?php if($timeslot != displayTimeSlots($confstarttime)) {
+                    //print(displayTimeSlots($confstarttime));
+                } else {
+                    //print ('<hr />');
+                }
+                $timeslot = displayTimeSlots($confstarttime);
+
+                ?>
+
+                <?php if ($cr3ativ_highlight != ('')){ ?>
+
+                <!--no highlight-->
+
+                <?php } else { ?>
+
+                    <?php $dateformat = get_option('date_format'); ?>
+                    <div class="row">
+                        <!-- Start of conference time -->
+                        <div class="conference-time one-sixth">
+                            <?php if ($confdisplaystarttime != ('')) { ?>
+
+                                <?php if ($confdisplaystarttime != ('')) { echo ($confdisplaystarttime); }
+                                if ($confdisplayendtime != ('')){ ?> &nbsp;-&nbsp; <?php echo ($confdisplayendtime); } ?>
+
+                            <?php } else { ?>
+
+                                <?php if ($confstarttime != ('')){  echo ($confstarttime); }
+                                if ($confendtime != ('')){ ?> &nbsp;-&nbsp; <?php echo ($confendtime); } ?>
+
+                            <?php } ?>
+                        </div>
+                        <!-- End of conference time -->
+
+                        <?php if ($sessiondate != (date_i18n($dateformat, $cr3ativconfmeetingdate))){ ?>
+                            <div class="five-sixths">
+                                <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php _e( 'Permanent Link to', 'cr3at_conf' ); ?>&nbsp;<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+                                <p class="sessioncats"><?php echo custom_taxonomies_terms_links(); ?></p><br />
+                            </div>
+                        <?php } else { ?>
+                            <?php if ( has_post_thumbnail() ) {  ?>
+                                <!-- Start of session featured image -->
+                                <div class="session_featured_image">
+                                    <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php _e( 'Permanent Link to', 'cr3at_conf' ); ?>&nbsp;<?php the_title_attribute(); ?>"><?php the_post_thumbnail(''); ?></a>
+                                </div><!-- End of session featured image -->
+                            <?php } ?>
+                            <h2 class="meeting_date"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php _e( 'Permanent Link to', 'cr3at_conf' ); ?>&nbsp;<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+                            <p><?php echo custom_taxonomies_terms_links(); ?></p>
+                        <?php } ?>
+                    </div>
+                 <?php } ?>
+            <?php endif; ?>
+        <?php endwhile; ?>
+
+    </div><!-- End of content wrapper -->
+
+    <!-- Clear Fix --><div class="cr3ativconference_clear"></div>
+
+</div><!-- End of content wrapper -->
+           
